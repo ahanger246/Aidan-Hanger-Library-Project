ccb9998f5714d8b094eb928f8bae0ff4d4517ff4
Aidan <ahanger@charlotte.edu>
2026-04-13 19:01:52 -0400

    Created server file to run server locally.

--- src/server.js
+++ src/server.js
@@ -1,1 +1,32 @@
-console.log('Hello, World!');
+import express from 'express';
+import morgan from 'morgan';
+import mediaRoutes from './routes/mediaRoutes.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+if(process.env.NODE_ENV !== 'test') app.use(morgan('tiny'));
+
+app.use('/api/media', mediaRoutes);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  if(!err.status) {
+    err.status = 500;
+    err.message ='Internal server error';
+  }
+  res.status(err.status).json({ error: err.message });
+});
+
+if(process.env.NODE_ENV !== 'test') {
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+}
+
+export default app;
